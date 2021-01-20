50b2cfce3da00a85275daf9304835568381ffbb7
Piotr Kowalski <perek.kowalski@gmail.com>
2021-01-20 23:00:49 +0100

    Initial commit

---
+++ exercise.js
@@ -0,0 +1,8 @@
+console.log('Hello world')
+const artykul = document.querySelector('.article_n2');
+
+
+console.log(artykul);
+console.log ('Dupa')
+const text = document.querySelector(".article_n2--js");
+console.log(text)
