e090b4b4e16e12c9ce78c9871d7ea46c3915808a
Piotr Kowalski <perek.kowalski@gmail.com>
2021-02-16 23:09:38 +0100

    Playing with functions

--- myscript.js
+++ myscript.js
@@ -1,5 +1,25 @@
-alert ('Welcome to my first webpage!')
+const myName = 'Jacek';
+const age = 29;
+
+console.log(age);
+
+function myFunction (myName, age)  {
+    console.log(`First parameter is ${myName}`);
+    console.log(`My secong parameter is ${age}`)
+    return (`Hello my name is ${myName} and I am ${age} years of age`);
+}
 
 
-const artykul = document.querySelector('nav');
-console.log(artykul);
+console.log(myFunction('Piotr',44))
+
+
+
+function calculate (inputNumber, otherNumber) {
+    return inputNumber+7+otherNumber
+}
+
+console.log(calculate(7,2))
+
+let foo = prompt(`What's your name?`);
+let bar = prompt(`How old are you`);
+console.log(myFunction(foo, bar));
